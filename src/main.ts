f01d40300fa830ecb06fe1e388a2bafbc431fc72
LeandroG04 <lgugiato@gmail.com>
2025-02-24 23:52:32 -0300

    Primer commit

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { TodoComponent } from './app/components/todo/todo.component';
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(TodoComponent, appConfig)
   .catch((err) => console.error(err));
