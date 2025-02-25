8dd17caf3763553328baa12c8857e91f8bf4ccee
LeandroG04 <lgugiato@gmail.com>
2025-02-25 02:20:40 -0300

    Auto-generated commit

--- src/main.server.ts
+++
@@ -1,7 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { TodoComponent } from './app/components/todo/todo.component';
-import { config } from './app/app.config.server';
-
-const bootstrap = () => bootstrapApplication(TodoComponent, config);
-
-export default bootstrap;
